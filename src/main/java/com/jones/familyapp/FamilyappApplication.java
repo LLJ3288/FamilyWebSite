97c69f8772f13eb80e884f849fd889851b1312b4
LLJ3288 <74459081+LLJ3288@users.noreply.github.com>
2021-03-24 11:26:01 -0500

    first commit

---
+++ src/main/java/com/jones/familyapp/FamilyappApplication.java
@@ -0,0 +1,13 @@
+package com.jones.familyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyappApplication.class, args);
+	}
+
+}
